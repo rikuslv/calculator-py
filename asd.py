ce9e6a9fe9853d7f613e63f16592d4254a4e2c00
Richards <62710433+rikuslv@users.noreply.github.com>
2025-07-13 19:28:22 +0300

    Add files via upload

---
+++ asd.py
@@ -0,0 +1,24 @@
+print("calculator on pythone")
+
+
+
+num = float(input("Enter first number: "))
+num1 = float(input("Enter secend number: "))
+
+op = input("Enter operation, + - * / ")
+
+if op == "+":
+    print("result", num + num1)
+    
+elif op == "*":
+    print("result", num * num1)
+
+elif op == "-":
+    print("result", num - num1)
+
+elif op == "/":
+    print("result", num / num1)
+
+else:
+    print("error")
+
